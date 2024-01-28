5c5162b025ef5076e146bf9b3339fec8fe4d0a98
leonel <leonellechuga38@gmail.com>
2024-01-28 17:18:13 -0600

    Apartado del video terminada

--- lib/pages/Paginavideo.dart
+++ lib/pages/Paginavideo.dart
@@ -1,13 +1,61 @@
-import 'package:flutter/material.dart';
-//pagina video
-class Paginavideo extends StatelessWidget{
- 
-  
-  @override
-  Widget build(BuildContext context){
-    return Center(
-      child: Text("Video", style: TextStyle(fontSize: 30),),
-    );
+// ignore_for_file: deprecated_member_use
 
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class Paginavideo extends StatefulWidget {
+  @override
+  _PaginavideoState createState() => _PaginavideoState();
+}
+
+class _PaginavideoState extends State<Paginavideo> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // Asegúrate de reemplazar 'URL_DEL_VIDEO' con la URL o la ruta de tu video.
+    _controller = VideoPlayerController.asset('assets/images/Video.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video'),
+      ),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : CircularProgressIndicator(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            // Puedes manejar la lógica de reproducción/pausa del video aquí
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              _controller.play();
+            }
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
