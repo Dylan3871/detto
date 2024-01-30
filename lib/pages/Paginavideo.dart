eddb7693100ce90ebe9815603560b54cd2ce76d1
leonel <leonellechuga38@gmail.com>
2024-01-29 19:33:09 -0600

    Apartado video

--- lib/pages/Paginavideo.dart
+++ lib/pages/Paginavideo.dart
@@ -1,74 +1,63 @@
-// // ignore_for_file: deprecated_member_use
+// ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api, prefer_const_constructors
 
 import 'package:flutter/material.dart';
-//pagina video
-class Paginavideo extends StatelessWidget{
- 
-  
+// ignore: depend_on_referenced_packages
+import 'package:video_player/video_player.dart';
+
+class Paginavideo extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
-    return Center(
-      child: Text("Video", style: TextStyle(fontSize: 30),),
-    );
+  _PaginavideoState createState() => _PaginavideoState();
+}
+
+class _PaginavideoState extends State<Paginavideo> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // Asegúrate de reemplazar 'URL_DEL_VIDEO' con la URL o la ruta de tu video.
+    _controller = VideoPlayerController.asset('assets/images/Video.mp4')
+  ..initialize().then((_) {
+    setState(() {});
+  });
 
   }
-} 
-// import 'package:flutter/material.dart';
-// import 'package:video_player/video_player.dart';
 
-// class Paginavideo extends StatefulWidget {
-//   @override
-//   _PaginavideoState createState() => _PaginavideoState();
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video'),
+      ),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : CircularProgressIndicator(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            // Puedes manejar la lógica de reproducción/pausa del video aquí
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              _controller.play();
+            }
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
 
-// class _PaginavideoState extends State<Paginavideo> {
-//   late VideoPlayerController _controller;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Asegúrate de reemplazar 'URL_DEL_VIDEO' con la URL o la ruta de tu video.
-//     _controller = VideoPlayerController.asset('VIDEO DET')
-//       ..initialize().then((_) {
-//         setState(() {});
-//       });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Video'),
-//       ),
-//       body: Center(
-//         child: _controller.value.isInitialized
-//             ? AspectRatio(
-//                 aspectRatio: _controller.value.aspectRatio,
-//                 child: VideoPlayer(_controller),
-//               )
-//             : CircularProgressIndicator(),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {
-//           setState(() {
-//             // Puedes manejar la lógica de reproducción/pausa del video aquí
-//             if (_controller.value.isPlaying) {
-//               _controller.pause();
-//             } else {
-//               _controller.play();
-//             }
-//           });
-//         },
-//         child: Icon(
-//           _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-//         ),
-//       ),
-//     );
-//   }
-
-//   @override
-//   void dispose() {
-//     super.dispose();
-//     _controller.dispose();
-//   }
-// }
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
